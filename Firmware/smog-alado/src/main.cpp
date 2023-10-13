dc542d78fec737e6e67a714fa560d95bba9c3e9c
machadoleonardo94 <52208834+machadoleonardo94@users.noreply.github.com>
2023-10-12 23:25:34 -0300

    First usable firmware upload
    * Declares inputs and outputs;
    * Calculates temperature from thermistor installed on A0 with 1k pulldown resistor;
    * Reads button presses to define preset heating temperatures

--- Firmware/smog-alado/src/main.cpp
+++ Firmware/smog-alado/src/main.cpp
@@ -1,18 +1,118 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+#define ledPin 2
+#define SDA 4
+#define SCL 5
+#define buttonPin 12
+#define heater 14
+
 
 // put function declarations here:
-int myFunction(int, int);
+double resCalc ();
+double steinhart (double termistor);
+void runHeater ( int preset);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(heater, OUTPUT);
+  pinMode(A0, INPUT);
+  digitalWrite(ledPin, LOW); //builtin LED set to ON on boot
+  digitalWrite(heater, LOW);  //heater set to OFF on boot
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);
 }
 
+bool state = false;
+uint16_t adcRaw = 0;
+double heaterTemperature = 0;
+int preset = 0;
+
+double adcTimer = 0;
+double loopTimer = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if ((millis()-adcTimer) > 1000){
+    adcTimer = millis();
+    heaterTemperature = steinhart(resCalc());
+    runHeater(preset);
+  }
+
+  if (!digitalRead(buttonPin)){
+    while(!digitalRead(buttonPin));
+    preset++;
+    Serial.printf("Clict Clect \n");
+    delay(20);
+    if (preset >= 4)
+      preset = 0;
+  }
+
+  if ((millis()-loopTimer) > 500){
+  loopTimer=millis();
+  digitalWrite(ledPin, state);
+  state = !(state);
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+double resCalc (){
+  int pulldownResistor = 1000;
+  float resistor = 0;
+  adcRaw = analogRead(A0);
+  Serial.print("ADC raw value: ");
+  Serial.println(adcRaw);
+  resistor = pulldownResistor * 1024 / (adcRaw-9) - pulldownResistor;
+  Serial.printf("Calculated resistor: %.1f \n", resistor);
+  return resistor;
+}
+
+double steinhart (double termistor){
+  const static double  a = 0.0008002314855002526;
+  const static double  b = 0.0001989545566222665;
+  const static double  c = 1.7249962319615102e-7;
+
+  // Utilizes the Steinhart-Hart Thermistor Equation:
+  // Temperature in Kelvin = 1 / {A + B[ln(R)] + C[ln(R)]^3}
+  double ln_r = log(termistor); // Saving the Log(resistance) so not to calculate it 4 times later.
+  float temperature = (1 / (a + (b * ln_r) + (c * ln_r * ln_r * ln_r))) - 273.15; //Convert K to ºC
+  Serial.printf("Temperature reading: %.1f \n", temperature);
+  return (temperature); 
+}
+
+void runHeater(int preset){
+  int power = 0;
+  int powerPercent = 0;
+  switch (preset){
+    case 0:
+      power = 0;
+      break;
+    case 1:
+      if (heaterTemperature < 100)
+        power = 50;
+      else
+        power = 0;;
+      break;
+    case 2:
+          if (heaterTemperature < 140)
+        power = 75;
+      else
+        power = 0;
+      break;
+    case 3: 
+      if (heaterTemperature < 140)
+        power = 100;
+      else
+        power = 0;
+      break;
+    default:
+      Serial.printf("No valid power option \n");
+  }
+  powerPercent = 100 * power/255;
+  Serial.printf ("PWM Heating %d \n", power);
+  Serial.printf ("PWM Heating %d%% \n", powerPercent);
+  if (power > 0)
+    analogWrite (heater, power);
+  else
+    digitalWrite (heater, LOW);
 }
