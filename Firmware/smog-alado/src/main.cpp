2e838a3e116ee493b3e506c50b915141dd238500
machadoleonardo94 <52208834+machadoleonardo94@users.noreply.github.com>
2023-10-24 14:45:04 -0300

    added proportional heating

--- Firmware/smog-alado/src/main.cpp
+++ Firmware/smog-alado/src/main.cpp
@@ -26,13 +26,15 @@ void setup() {
 
 bool state = false;
 double heaterTemperature = 0;
+double heaterOld = 0;
+int tempGoal = 200;
 int preset = 0;
 
 double adcTimer = 0;
 double loopTimer = 0;
 
 void loop() {
-  if ((millis()-adcTimer) > 1000){
+  if ((millis()-adcTimer) > 200){
     adcTimer = millis();
     heaterTemperature = steinhart(resCalc());
     runHeater(preset);
@@ -42,7 +44,7 @@ void loop() {
     while(!digitalRead(buttonPin));
     preset++;
     Serial.printf("Clict Clect \n");
-    delay(20);
+    delay(100);
     if (preset >= 4)
       preset = 0;
   }
@@ -85,27 +87,32 @@ double steinhart (double termistor){
 void runHeater(int preset){
   int power = 0;
   int powerPercent = 0;
+  int pValue = 3;
+  int iValue = 2;
   switch (preset){
     case 0:
       power = 0;
       break;
     case 1:
-      if (heaterTemperature < 100)
+      if (heaterTemperature < 140)
         power = 50;
       else
         power = 0;;
       break;
     case 2:
-          if (heaterTemperature < 140)
-        power = 75;
+          if (heaterTemperature < 180)
+        power = 100;
       else
         power = 0;
       break;
     case 3: 
-      if (heaterTemperature < 140)
-        power = 100;
-      else
-        power = 0;
+      if (heaterTemperature < 230){
+        if (tempGoal - heaterTemperature > 20)
+          power = 150;
+        else {
+          power = 150 + pValue * (tempGoal - heaterTemperature);
+        }
+      } 
       break;
     default:
       Serial.printf("No valid power option \n");
