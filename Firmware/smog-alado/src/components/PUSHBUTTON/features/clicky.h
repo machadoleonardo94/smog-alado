38562c63c3683b6352c5356bfd5fb9fa396239f5
machadoleonardo94 <52208834+machadoleonardo94@users.noreply.github.com>
2023-11-23 20:50:45 -0300

    Moved click function to dedicated lib

--- Firmware/smog-alado/src/components/PUSHBUTTON/features/clicky.h
+++ Firmware/smog-alado/src/components/PUSHBUTTON/features/clicky.h
@@ -8,17 +8,39 @@
 int buttonPress(int button)
 {
   int count = 0;
+  int state = 0;
   while (!digitalRead(button))
   {
     count++;
     delay(1);
   }
-  if (count > 1000)
-    return 2;
-  if (count > 20)
-    return 1; 
-  else
+  if (count < 10)
     return 0;
+  if (count > 20)
+    state = 1; 
+  if (count > 1000)
+    state = 2;
+  
+  if (state == 1)
+  {
+    preset++;   
+    if (preset >= 12)
+      preset = 0;
+    if (sleepy == true)
+    {
+      preset = 0;
+      display.dim(false);
+      sleepy = false;
+    }  
+    Serial.printf("Clict Clect \n");
+    idleTimer = 0;
+  }
+  if (state == 2)
+  {
+    sleepRoutine();
+  }
+  delay(100);
+  return 1;
 }
 
 #endif // PUSHBUTTON
