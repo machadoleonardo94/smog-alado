f518ed34b5f991d2f74371971fa9805c6b958a5c
machadoleonardo94 <52208834+machadoleonardo94@users.noreply.github.com>
2023-11-23 20:49:29 -0300

    Moved dependencies to correct folder

--- Firmware/smog-alado/src/shared/dependencies.h
+++ Firmware/smog-alado/src/shared/dependencies.h
@@ -17,8 +17,23 @@
 //* ---------------------- Libraries --------------------------
 //* ADS1115 analog to digital converter and other dependencies:
 #include <SPI.h>
+#include <Wire.h>
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_ADS1X15.h>
+#include <EEPROM.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <TelnetStream.h>
+#include <TimeLib.h>
+#include <sntp.h>
+#include <TZ.h>
 
 //* ---------------------- Constants --------------------------
 #include "constants.h"
