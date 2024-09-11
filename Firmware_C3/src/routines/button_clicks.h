f6bd8baf58af90c020950dd2d169643af7b04701
machadoleonardo94 <52208834+machadoleonardo94@users.noreply.github.com>
2024-09-11 12:39:02 -0300

    Updated buttonPress routine

--- Firmware_C3/src/routines/button_clicks.h
+++ Firmware_C3/src/routines/button_clicks.h
@@ -1,85 +1,26 @@
 #include "shared/dependencies.h"
+// #include "routines/update_screen.h"
+
+void shutdownESP();
+void buttonPress();
 
 void buttonPress()
 {
   if (digitalRead(buttonPin))
   {
-    int shutdownCounter = 0;
-    while (digitalRead(buttonPin))
-    {
-      sampleRandomLED();
-      shutdownCounter++;
-      if (shutdownCounter > 100)
-      {
-        digitalWrite(latchPin, LOW);
-        setLED(0, 0, 0);
-        esp_deep_sleep_start();
-      }
-      delay(25);
-    }
+    burnoutCounter++;
     idleTimer = 0;
-  }
-}
-
-int buttonPress(int button)
-{
-  int count = 0;
-  int state = 0;
-  while (!digitalRead(button))
-  {
-    count++;
-    delay(1);
-  }
-  if (count < 10)
-    return 0;
-  if (count > 20)
-    state = 1;
-  if (count > 800)
-    state = 2;
-  if (count > 5000)
-    state = 3;
-  if (count > 10000)
-    state = 4;
-
-  if (state == 1)
-  {
-    preset++;
-    if (preset >= 10)
-      preset = 0;
-    if (sleepy == true)
+    if (buttonState == false)
     {
-      sleepy = false;
-      preset = 0;
-      display.dim(false);
-      // WiFi.forceSleepWake();
-      // WiFi.begin();
+      clickCounter++;
+      buttonState = true;
     }
-    Serial.printf("Clict Clect \n");
-    idleTimer = 0;
-    if (preset == 0)
-      tempGoal = 0;
-    if (preset == 1)
-      tempGoal = 100;
-    if (preset == 2)
-      tempGoal = 120;
-    if ((preset > 2) & (preset < 10))
-      tempGoal = 110 + (preset * 5);
   }
-  if (state == 2)
+  else
   {
-    // sleepRoutine();
+    burnoutCounter = 0;
+    if (buttonState)
+      delay(50); // debounce
+    buttonState = false;
   }
-  if (state == 3)
-  {
-    // TelnetStream.println("Tuning PID Parameters");
-    // changeAutoTune();  // Call the autoTunePID function for tuning logic
-  }
-  if (state == 4)
-  {
-    display.clearDisplay();
-    display.display();
-    ESP.restart();
-  }
-  delay(100);
-  return 1;
 }
