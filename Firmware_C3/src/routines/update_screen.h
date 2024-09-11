6f00888ba87803933232e49b6d1c632b8712ea4d
machadoleonardo94 <52208834+machadoleonardo94@users.noreply.github.com>
2024-09-11 12:39:30 -0300

    Added screen routine

--- Firmware_C3/src/routines/update_screen.h
+++ Firmware_C3/src/routines/update_screen.h
@@ -8,12 +8,49 @@
 #define _width 128
 #define _height 64
 
+void updateDisplay();
+
 void updateDisplay()
 {
   int remainingTime = (TIME_TO_SLEEP - idleTimer) / SAMPLING_TIMER;
   idleMinutes = remainingTime / 600;
   idleSeconds = (remainingTime % 600) / 10;
 
+  display.clearDisplay();
+  display.setTextSize(1); // Set text size to 2 (you can adjust the size as needed)
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(102, LINE0);
+  display.print(idleMinutes);
+  display.print(":");
+  if (idleSeconds < 10)
+    display.print("0");
+  display.print(idleSeconds);
+
+  display.setCursor(0, LINE1); // Adjust vertical position
+  display.print("PWM: ");
+  display.print(powerPercent);
+  display.print("%");
+  display.setCursor(92, LINE1);
+  display.print(battery);
+  display.print("V");
+
+  // Display the IP address
+  display.setCursor(0, LINE3); // Adjust vertical position
+  display.print("IP: ");
+  display.print(WiFi.localIP());
+
+  display.display();
+}
+
+void burnoutScreen()
+{
+  display.clearDisplay();
+  drawXbm(logo_bits, _width, _height, 0, 0);
+  delay(3000);
+}
+
+void updateDisplayTemps()
+{
   display.clearDisplay();
   display.setTextSize(1); // Set text size to 2 (you can adjust the size as needed)
   display.setTextColor(SSD1306_WHITE);
@@ -34,26 +71,5 @@ void updateDisplay()
   display.print(tempGoal);
   display.print((char)247);
   display.print("C");
-
-  display.setCursor(0, LINE2); // Adjust vertical position
-  display.print("PWM: ");
-  display.print(powerPercent);
-  display.print("%");
-  display.setCursor(92, LINE2);
-  display.print(battery);
-  display.print("V");
-  /*
-  display.setCursor(102, LINE2);
-  display.print(idleMinutes);
-  display.print(":");
-  if (idleSeconds < 10)
-    display.print("0");
-  display.print(idleSeconds);
-  */
-  // Display the IP address
-  display.setCursor(0, LINE3); // Adjust vertical position
-  display.print("IP: ");
-  display.print(WiFi.localIP());
-
   display.display();
 }
